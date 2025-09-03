7ce0c44337a8d82a6b365088fc9a7be683bf72f3
Er Ma <erma@placeholder.com>
2025-09-03 13:45:40 +0800

    chore(Camera): 更正摄像机缩放组件序列化参数的描述；

--- camera_2d/zoom/camera_2d_zoom_component.gd
+++ camera_2d/zoom/camera_2d_zoom_component.gd
@@ -7,8 +7,8 @@ class_name Camera2DZoomComponent
 @export_enum("Lerp", "Smooth") var motion_mode: int = 0 ## 摄像机的缩放模式
 @export var target_zoom: Vector2 = Vector2(1.0, 1.0) ## 摄像机目标缩放倍率
 @export_range(0.01, 1.00, 0.01, "suffix:倍率/步") var step_size: float = 0.1 ## 摄像机的缩放步长
-@export_range(0.1, 1.0, 0.1, "suffix:倍率") var min_zoom: float = 0.7 ## 摄像机的缩放步长
-@export_range(1.0, 5.0, 0.1, "suffix:倍率") var max_zoom: float = 3 ## 摄像机的缩放步长
+@export_range(0.1, 1.0, 0.1, "suffix:倍率") var min_zoom: float = 0.7 ## 摄像机的最小缩放倍率
+@export_range(1.0, 5.0, 0.1, "suffix:倍率") var max_zoom: float = 3 ## 摄像机的最大缩放倍率
 @export_range(1.0, 10.0, 0.1) var deceleration_speed: float = 5.0 ## 摄像机的插值移动速度
 @export_range(1.0, 1000.0, 1.0, "suffix:倍率/s") var smoothing_speed: float = 0.1 ## 摄像机的平滑移动速度
 
