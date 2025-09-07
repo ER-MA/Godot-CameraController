136b5da3ee6763c6d368560804493b07ce56661d
Er Ma <erma@placeholder.com>
2025-09-07 10:20:38 +0800

    fix(InputRemapper): 完善自定义按键功能，增强健壮性，修复按键处理优先级问题；

--- remapper/input_mapper.gd
+++ remapper/input_mapper.gd
@@ -2,40 +2,45 @@ extends Control
 class_name InputMapper
 
 const  KEYMAPS_PATH = "user://keymaps.dat" # 按键映射文件的保存路径（需要在高级设置的“应用->配置”中开启和设置用户自定义目录）
-var keymaps: Dictionary
+var keymaps: Dictionary # 当前项目中所有动作和事件数组的键值对（动作数量取决于该脚本实例化时编辑器中的动作数量）
 
+
+func _init() -> void:
+	# 缓存编辑器中的所有动作
+	_update_keymaps()
+	# 加载本地缓存的按键映射（子节点会自己在ready阶段更新文本）
+	load_keymaps()
 
 func _ready() -> void:
+	pass
+
+
+func _update_keymaps() -> void:
 	for action in InputMap.get_actions(): # 获取当前项目中所有的动作
-		if InputMap.action_get_events(action).size() != 0: # 若动作中存在事件
-			keymaps[action] = InputMap.action_get_events(action)[0] # 则将动作添加为键，事件赋为值
-	# 加载本地缓存的按键映射
-	load_keymaps()
-	# 加载完成后让所有子节点更新文本（危险！！！）
-	for button in get_children():
-		button.update_text()
+		keymaps[action] = InputMap.action_get_events(action) # 则将动作添加为键，事件赋为值
+		# （不管返回的事件数组是否为空，先把动作存上，以防止编辑器中的动作未配置事件导致本地事件无法被加载）
 
 
 func load_keymaps() -> void:
 	if FileAccess.file_exists(KEYMAPS_PATH): # 若文件存在于路径中
+		# 开启并读取数据
 		var file: FileAccess = FileAccess.open(KEYMAPS_PATH, FileAccess.READ)
 		var local_keymaps: Dictionary = file.get_var(true) as Dictionary
 		file.close()
-		# 替换为加载的本地按键映射
+		
+		# 替换按键映射
 		for action in keymaps.keys():
 			if local_keymaps.has(action):
 				InputMap.action_erase_events(action)
-				InputMap.action_add_event(action, local_keymaps[action])
-				keymaps[action] = InputMap.action_get_events(action)[0]
-				# 另一种写法，但是感觉没上面这种靠谱
-				#keymaps[action] = local_keymaps[action]
-				#InputMap.action_erase_events(action)
-				#InputMap.action_add_event(action, keymaps[action])
-		
+				for event in local_keymaps[action]:
+					InputMap.action_add_event(action, event)
+				keymaps[action] = InputMap.action_get_events(action)
+			
 	else: # 若文件不存在
-		save_keymaps()
+		save_keymaps() # 保存按键映射缓存到本地
 
 func save_keymaps() -> void:
+	_update_keymaps() # 先从编辑器更新，再缓存
 	var file: FileAccess = FileAccess.open(KEYMAPS_PATH, FileAccess.WRITE)
 	file.store_var(keymaps, true)
 	file.close()
--- remapper/remapper.gd
+++ remapper/remapper.gd
@@ -1,32 +1,38 @@
 extends Button
 
 @export var input_mapper: InputMapper
-@export var action: String
+@export var action: String ## 操作的动作
+@export var event_num: int ## 操作动作的第 num 个事件
 
 
 func _init() -> void:
-	toggle_mode = true
-
+	toggle_mode = true # 设置按钮为切换模式
+	
 func _ready() -> void:
-	set_process_unhandled_input(false)
+	update_text() # 更新文本（权宜之计，需要确保InputMapper比这里更早读取本地按键映射）
 
 func _toggled(toggled_on: bool) -> void:
-	set_process_unhandled_input(toggled_on)
 	if toggled_on:
+		grab_focus()  # 确保获得焦点
 		text = "等待输入"
+	else:
+		release_focus() # 放弃焦点
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_pressed():
+func _gui_input(event: InputEvent) -> void: # 使用 _gui_input 确保所有键盘事件会优先进入此方法
+	if event.is_pressed() and button_pressed:
+		# 处理输入事件
 		InputMap.action_erase_events(action) # 从 action 对应的动作中擦除所有已绑定的事件
 		InputMap.action_add_event(action, event) # 将接收到的事件添加至 action 对应的动作中
-		button_pressed = false
-		release_focus() # 放弃焦点
-		update_text()
-		# 保存修改
-		# ...
-		# 另一种写法，但是依然感觉没上面这种靠谱
-		input_mapper.keymaps[action] = event
-		input_mapper.save_keymaps()
+		# 更新状态
+		button_pressed = false # 关闭按钮（在 _toggle() 函数中会自动放弃焦点）
+		update_text() # 更新文本
+		input_mapper.save_keymaps() # 保存修改
+		# 阻止事件继续传播
+		accept_event()
 
 func update_text() -> void:
-	text = InputMap.action_get_events(action)[0].as_text() # 从 action 对应的动作数组中读取第一个事件，并将其转为文本
+	var events = InputMap.action_get_events(action)
+	if events.size() > event_num:
+		text = events[event_num].as_text()
+	else:
+		text = "未绑定"
