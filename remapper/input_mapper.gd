43e164e6fa00a775fba99bf804101bcbf112ee18
Er Ma <erma@placeholder.com>
2025-09-07 20:19:29 +0800

    feat(InputRemapper): 完善错误处理机制；

--- remapper/input_mapper.gd
+++ remapper/input_mapper.gd
@@ -3,57 +3,76 @@ class_name InputMapper
 
 signal action_changed(action: String) # 持有该节点的按钮通过这个信号告诉所有人哪个动作被更新了
 
+@export var is_enable: bool = true
 @export var keymaps_path: String = "user://keymaps.dat" ## 按键映射文件的保存路径（需要在高级设置的“应用->配置”中开启和设置用户自定义目录）
 var keymaps: Dictionary # 缓存当前项目中所有动作和事件数组的键值对（动作数量取决于该脚本实例化时编辑器中的动作数量）
 
 
 func _ready() -> void:
-	load_keymaps() # 加载本地缓存的按键映射（子节点会自己在ready阶段更新文本）
-	# 持有本节点的 remapper 脚本会在 ready 信号发出后更新所有文本
-	# 持有本节点的 remapper 脚本会在 action_changed 信号发出后更新对应 action 的文本
-	action_changed.connect(_on_action_changed) # 由本脚本将更新后的按键映射保存至本地
+	if is_ready():
+		load_keymaps() # 加载本地缓存的按键映射（子节点会自己在ready阶段更新文本）
+		# 持有本节点的 remapper 脚本会在 ready 信号发出后更新所有文本
+		# 持有本节点的 remapper 脚本会在 action_changed 信号发出后更新对应 action 的文本
+		action_changed.connect(_on_action_changed) # 由本脚本将更新后的按键映射保存至本地
 
 
 func _on_action_changed(_action: String) -> void:
-	save_keymaps() # remapper 脚本更新后通知本脚本覆盖保存按键映射
+	if is_enable:
+		save_keymaps() # remapper 脚本更新后通知本脚本覆盖保存按键映射
 
 
 func _update_keymaps() -> void:
-	for action in InputMap.get_actions(): # 获取当前项目中所有的动作
-		keymaps[action] = InputMap.action_get_events(action) # 则将动作添加为键，事件赋为值
-		# （不管返回的事件数组是否为空，先把动作存上，以防止编辑器中的动作未配置事件导致本地事件无法被加载）
+	if is_enable:
+		for action in InputMap.get_actions(): # 获取当前项目中所有的动作
+			keymaps[action] = InputMap.action_get_events(action) # 则将动作添加为键，事件赋为值
+			# （不管返回的事件数组是否为空，先把动作存上，以防止编辑器中的动作未配置事件导致本地事件无法被加载）
 
 
 func load_keymaps() -> void:
-	# 缓存编辑器中的所有动作到 keymaps
-	_update_keymaps()
-	
-	# 若文件存在于路径中
-	if FileAccess.file_exists(keymaps_path):
-		# 开启并读取数据
-		var file: FileAccess = FileAccess.open(keymaps_path, FileAccess.READ)
-		var local_keymaps: Dictionary = file.get_var(true) as Dictionary
-		file.close()
+	if is_enable and keymaps_path:
+		# 缓存编辑器中的所有动作到 keymaps
+		_update_keymaps()
 		
-		# 替换按键映射
-		for action in keymaps.keys():
-			if local_keymaps.has(action):
-				InputMap.action_erase_events(action)
-				for event in local_keymaps[action]:
-					InputMap.action_add_event(action, event)
-				keymaps[action] = InputMap.action_get_events(action)
-				
-	# 若文件不存在
-	else:
-		# 保存按键映射到本地
-		save_keymaps() 
-		
+		# 若文件存在于路径中
+		if FileAccess.file_exists(keymaps_path):
+			# 开启并读取数据
+			var file: FileAccess = FileAccess.open(keymaps_path, FileAccess.READ)
+			var local_keymaps: Dictionary = file.get_var(true) as Dictionary
+			file.close()
+			
+			# 替换按键映射
+			for action in keymaps.keys():
+				if local_keymaps.has(action):
+					InputMap.action_erase_events(action)
+					for event in local_keymaps[action]:
+						InputMap.action_add_event(action, event)
+					keymaps[action] = InputMap.action_get_events(action)
+					
+		# 若文件不存在
+		else:
+			# 保存按键映射到本地
+			save_keymaps() 
+			
 
 func save_keymaps() -> void:
-	# 先从编辑器更新按键映射缓存 keymaps
-	_update_keymaps()
-	# 再写入文件
-	var file: FileAccess = FileAccess.open(keymaps_path, FileAccess.WRITE)
-	file.store_var(keymaps, true)
-	file.close()
-	print("save_keymaps")
+	if is_enable and keymaps_path:
+		# 先从编辑器更新按键映射缓存 keymaps
+		_update_keymaps()
+		# 再写入文件
+		var file: FileAccess = FileAccess.open(keymaps_path, FileAccess.WRITE)
+		file.store_var(keymaps, true)
+		file.close()
+		print("save_keymaps")
+
+
+func is_ready() -> bool:
+	# 组件自检
+	if not is_enable:
+		push_warning("[%s] 此脚本被禁用" % self.get_script().get_global_name())
+		return false
+		
+	if not keymaps_path:
+		push_error("[%s] 未配置按键映射文件的保存路径" % self.get_script().get_global_name())
+		return false
+		
+	return true
--- remapper/remapper.gd
+++ remapper/remapper.gd
@@ -1,6 +1,7 @@
 extends Button
 class_name Remapper
 
+@export var is_enable: bool = true
 @export var input_mapper: InputMapper
 @export var rebind_action: String ## 该按钮要重绑定的动作
 @export var event_num: int ## 该按钮要绑定的动作的第 num 个事件
@@ -15,70 +16,106 @@ static var active_remapper: Remapper = null # 静态变量跟踪当前激活的r
 
 
 func _init() -> void:
-	toggle_mode = true # 设置按钮为切换模式
+	if is_enable:
+		self.toggle_mode = true # 设置按钮为切换模式
 
 func _ready() -> void:
-	input_mapper.ready.connect(update_all_text) # 链接事件更新信号到文本更新函数（使 InputMapper 准备好后再执行初始化）
-	input_mapper.action_changed.connect(update_text) # 链接事件更新信号到文本更新函数
+	if is_ready():
+		input_mapper.ready.connect(update_all_text) # 链接事件更新信号到文本更新函数（使 InputMapper 准备好后再执行初始化）
+		input_mapper.action_changed.connect(update_text) # 链接事件更新信号到文本更新函数
 
 func _toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		# 关闭前一个激活的 remapper（防止出现多个按钮被同时激活的情况）
-		if Remapper.active_remapper and Remapper.active_remapper != self:
-			Remapper.active_remapper._cancel_binding()
-		Remapper.active_remapper = self
-		grab_focus()  # 确保获得焦点
-		text = wait_input
-	else:
-		if Remapper.active_remapper == self:
-			Remapper.active_remapper = null
-		release_focus() # 放弃焦点
+	if is_enable:
+		if toggled_on:
+			# 关闭前一个激活的 remapper（防止出现多个按钮被同时激活的情况）
+			if Remapper.active_remapper and Remapper.active_remapper != self:
+				Remapper.active_remapper._cancel_binding()
+			Remapper.active_remapper = self
+			grab_focus()  # 确保获得焦点
+			text = wait_input
+		else:
+			if Remapper.active_remapper == self:
+				Remapper.active_remapper = null
+			release_focus() # 放弃焦点
 
 func _gui_input(event: InputEvent) -> void: # 使用 _gui_input 确保所有键盘事件会优先进入此方法
-	if event.is_pressed() and button_pressed and InputMap.has_action(rebind_action):
-		# 处理输入事件
-		
-		# 填充模式
-		if display_mode == 0:
-			if InputMap.action_get_events(rebind_action).size() > event_num: # 若当前按钮对应的事件存在则擦除事件
-				InputMap.action_erase_event(rebind_action, InputMap.action_get_events(rebind_action)[event_num]) # 从 action 对应的动作中擦除当前绑定的事件
-			if not event.is_action(cancel_action): # 若为取消键则不添加事件
-				InputMap.action_add_event(rebind_action, event) # 将接收到的事件添加至 action 对应的动作中
+	if is_enable and rebind_action and InputMap.has_action(rebind_action) and cancel_action:
+		if event.is_pressed() and self.button_pressed:
+			# 处理输入事件
 			
-		# 滞留模式
-		elif display_mode == 1:
-			# ！！！无效！！！（InputMap 似乎不会接收空的 InputEvent。想要实现该功能应该还是需要修改文本显示部分的函数）
-			var events: Array = InputMap.action_get_events(rebind_action)
-			var empty_event: InputEvent = InputEventKey.new()
-			# 当事件数量不足时使用空事件扩展数组
-			while events.size() <= event_num:
-				events.append(empty_event)
-			# 替换当前按钮的事件为玩家输入的事件
-			if event.is_action(cancel_action):
-				events[event_num] = empty_event
-			else:
-				events[event_num] = event
-			# 按顺序重新写入事件到动作
-			InputMap.action_erase_events(rebind_action) # 清除动作中的所有事件
-			for e in events:
-				InputMap.action_add_event(rebind_action, e)
-				print(e)
+			# 填充模式
+			if display_mode == 0:
+				if InputMap.action_get_events(rebind_action).size() > event_num: # 若当前按钮对应的事件存在则擦除事件
+					InputMap.action_erase_event(rebind_action, InputMap.action_get_events(rebind_action)[event_num]) # 从 action 对应的动作中擦除当前绑定的事件
+				if not event.is_action(cancel_action): # 若为取消键则不添加事件
+					InputMap.action_add_event(rebind_action, event) # 将接收到的事件添加至 action 对应的动作中
 				
-		_cancel_binding() # 更新状态
-		accept_event() # 阻止事件继续传播
+			# 滞留模式
+			elif display_mode == 1:
+				# ！！！无效！！！（InputMap 似乎不会接收空的 InputEvent。想要实现该功能应该还是需要修改文本显示部分的函数）
+				var events: Array = InputMap.action_get_events(rebind_action)
+				var empty_event: InputEvent = InputEventKey.new()
+				# 当事件数量不足时使用空事件扩展数组
+				while events.size() <= event_num:
+					events.append(empty_event)
+				# 替换当前按钮的事件为玩家输入的事件
+				if event.is_action(cancel_action):
+					events[event_num] = empty_event
+				else:
+					events[event_num] = event
+				# 按顺序重新写入事件到动作
+				InputMap.action_erase_events(rebind_action) # 清除动作中的所有事件
+				for e in events:
+					InputMap.action_add_event(rebind_action, e)
+					print(e)
+					
+			_cancel_binding() # 更新状态
+			accept_event() # 阻止事件继续传播
 
 
 func _cancel_binding():
-	button_pressed = false # 关闭按钮（会触发 _toggle() 函数，并在其中放弃焦点）
-	input_mapper.action_changed.emit(rebind_action) # 发出信号，让所有同 action 的按钮更新文本，让 InputMapper 将修改后的按键映射保存到本地（虽然其可能并没有被更改，不过问题不大）
+	if is_enable and input_mapper and rebind_action:
+		self.button_pressed = false # 关闭按钮（会触发 _toggle() 函数，并在其中放弃焦点）
+		input_mapper.action_changed.emit(rebind_action) # 发出信号，让所有同 action 的按钮更新文本，让 InputMapper 将修改后的按键映射保存到本地（虽然其可能并没有被更改，不过问题不大）
 
 
 func update_text(action: String) -> void:
-	if action == rebind_action:
-		if InputMap.action_get_events(action).size() > event_num:
-			text = InputMap.action_get_events(action)[event_num].as_text()
-		else:
-			text = unbind
+	if is_enable and rebind_action:
+		if action == rebind_action:
+			if InputMap.action_get_events(action).size() > event_num:
+				text = InputMap.action_get_events(action)[event_num].as_text()
+			else:
+				text = unbind
 	
 func update_all_text() -> void:
-	update_text(rebind_action)
+	if is_enable and rebind_action:
+		update_text(rebind_action)
+
+
+func is_ready() -> bool:
+	# 组件自检
+	if not is_enable:
+		push_warning("[%s] 此脚本被禁用" % self.get_script().get_global_name())
+		return false
+		
+	if not input_mapper:
+		push_error("[%s] 未分配或找到 InputMapper" % self.get_script().get_global_name())
+		return false
+		
+	if not input_mapper.is_ready():
+		push_warning("[%s] 依赖的组件 InputMapper 尚未就绪" % self.get_script().get_global_name())
+		return false
+		
+	if not rebind_action:
+		push_error("[%s] 未配置该按钮要重绑定的动作" % self.get_script().get_global_name())
+		return false
+		
+	if not InputMap.has_action(rebind_action):
+		push_error("[{0}] 输入映射中未找到动作 {1}".format([self.get_script().get_global_name(), rebind_action]))
+		return false
+		
+	if not cancel_action:
+		push_error("[%s] 未配置该按钮取消绑定用的动作" % self.get_script().get_global_name())
+		return false
+		
+	return true
